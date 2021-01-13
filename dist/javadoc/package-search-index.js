10dbe9da72e329b5df1a63d5cc1bbab36689acc9
AbhishekRoy14 <66768294+AbhishekRoy14@users.noreply.github.com>
2021-01-13 21:44:26 +1300

    Add files via upload

---
+++ dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"core.buyMoreCoord"},{"l":"core.inventory"},{"l":"core.parameters"},{"l":"core.sale"},{"l":"core.test"},{"l":"gui"}]
